547ac81459d4aa31ef0a7a81c29bc623aca5a9e0
Bird Michael GBJH <Michael.Bird@syngenta.com>
2023-07-06 10:36:09 +0100

    v3

--- app.R
+++ app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("HW for gitlab version 2"),
+    titlePanel("HW for gitlab version 3"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
